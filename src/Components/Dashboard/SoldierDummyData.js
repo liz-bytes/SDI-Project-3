35b269696c745867d0086e75a2f2e630ca768f2d
NoofleBot <nooflebot@gmail.com>
2025-03-21 13:23:08 -0700

    Added filters for equipment and division commander

---
+++ src/Components/Dashboard/SoldierDummyData.js
@@ -0,0 +1,22 @@
+function SoldierDummyData () {
+
+const DummyData = [
+{ first_name: 'Michael', last_name: 'Thompson', id_deployments: ['Operation Chrono Storm'], home_unit: ['Deep Space Temporal Command Center'], id_mos: ['Dimensional Commander'] },
+{ first_name: 'Elizabeth', last_name: 'Santiago', id_deployments: ['Operation Paradox Rift'], home_unit: ['Temporal Vanguard Division'], id_mos: ['Dimensional Commander'] },
+{ first_name: 'Devrin', last_name: 'Nguyen', id_deployments: ['Operation Obsidian Ghost'], home_unit: ['Void Operations and Reconnaissance Group'], id_mos: ['Dimensional Commander'] },
+
+// Additional Soldiers
+{ first_name: 'Ryan', last_name: 'Cole', id_deployments: ['Operation Rift Sentinel'], home_unit: ['1st Shadow Brigade'], id_mos: ['Dimensional Paratrooper'] },
+{ first_name: 'Ava', last_name: 'Montoya', id_deployments: ['Operation Nexus Breaker'], home_unit: ['35th Nexus HyperNet Battalion'], id_mos: ['Dimensional IT Specialist'] },
+{ first_name: 'Jordan', last_name: 'Xiang', id_deployments: ['Operation Celestial Beacon'], home_unit: ['2-503rd Shock IN Battalion'], id_mos: ['Dimensional Armor'] },
+{ first_name: 'Isabella', last_name: 'Carter', id_deployments: ['Operation Eclipse Watch'], home_unit: ['Deep Space Temporal Command Center'], id_mos: ['Dimensional Transport'] },
+{ first_name: 'Dante', last_name: 'Vega', id_deployments: ['Operation Abyss Walker'], home_unit: ['Void Operations and Reconnaissance Group'], id_mos: ['Dimensional Operator'] },
+{ first_name: 'Sophia', last_name: 'Hale', id_deployments: ['Operation Silent Horizon'], home_unit: ['1st Shadow Brigade'], id_mos: ['Dimensional Supply Specialist'] },
+{ first_name: 'Ethan', last_name: 'Reed', id_deployments: ['Operation Phantom Veil'], home_unit: ['157th Oblivion Wing'], id_mos: ['Dimensional Starship Pilot'] },
+{ first_name: 'Liam', last_name: 'Takahashi', id_deployments: ['Operation Titan’s Descent'], home_unit: ['35th Nexus HyperNet Battalion'], id_mos: ['Dimensional Starship Mechanic'] },
+{ first_name: 'Emily', last_name: 'Ortega', id_deployments: ['Operation Void Genesis'], home_unit: ['2-503rd Shock IN Battalion'], id_mos: ['Dimensional Starship Door Gunner'] }
+]
+return DummyData;
+};
+
+export default SoldierDummyData;
