db392a5477c618873d1ed2484645bb5456a7e857
Liz <lizmarie.mendez86@gmail.com>
2025-03-24 16:25:38 -0400

    added new Context Hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ import BDE_Dashboard from './Components/Dashboard/BDE_Commander.jsx';
 import Div_Dashboard from './Components/Dashboard/Div_Commander.jsx';
 import PageNotFound from './Components/PageNotFound/PageNotFound.jsx';
 import Equipment_Data from './Components/Dashboard/Equipment/EquipmentDashBoard.jsx';
+import { SoldierProvider } from './Components/Dashboard/SoldierProvider.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/BN_Dashboard" element={<BN_Dashboard />} />
-        <Route path="/BDE_Dashboard" element={<BDE_Dashboard />} />
-        <Route path="/Div_Dashboard" element={<Div_Dashboard />} />
-        <Route path="/Equipment_Data" element={<Equipment_Data />} />
-      </Routes>
-    </BrowserRouter>
+    <SoldierProvider>
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/BN_Dashboard" element={<BN_Dashboard />} />
+          <Route path="/BDE_Dashboard" element={<BDE_Dashboard />} />
+          <Route path="/Div_Dashboard" element={<Div_Dashboard />} />
+          <Route path="/Equipment_Data" element={<Equipment_Data />} />
+        </Routes>
+      </BrowserRouter>
+    </SoldierProvider>
   );
 }
 
