434b1ae921ad2a1ea40d38552554cc6fde312763
NoofleBot <nooflebot@gmail.com>
2025-03-24 13:35:50 -0700

    Merge

--- src/Components/Dashboard/Div_Commander.jsx
+++ src/Components/Dashboard/Div_Commander.jsx
@@ -40,7 +40,6 @@ function Div_Dashboard() {
   return (
     <div>
       <h2>Soldier Status</h2>
-
       {/* Container for "Filter by:" label and the dropdown */}
       <div className="filter-section">
         <h3>Filter by:</h3>
@@ -56,46 +55,44 @@ function Div_Dashboard() {
           <option value="mos">MOS</option>
         </select>
       </div>
+      <div className="results-container">
+        {/* Only show the text input and clear button if a filter category is selected */}
+        {filterCategory && (
+          <div className="filter-input-group">
+            <input
+              type="text"
+              placeholder={`Enter ${filterCategory} to filter`}
+              value={filterValue}
+              onChange={(e) => setFilterValue(e.target.value)}
+            />
+            <button onClick={() => { setFilterCategory(''); setFilterValue(''); }}>
+              Clear Filter
+            </button>
+          </div>
+        )}
 
-      {/* Only show the text input and clear button if a filter category is selected */}
-      {filterCategory && (
-        <div className="filter-input-group">
-          <input
-            type="text"
-            placeholder={`Enter ${filterCategory} to filter`}
-            value={filterValue}
-            onChange={(e) => setFilterValue(e.target.value)}
-          />
-          <button onClick={() => {
-            setFilterCategory('');
-            setFilterValue('');
-          }}>
-            Clear Filter
-          </button>
-        </div>
-      )}
-
-      <br />
-      <table>
-        <thead>
-          <tr>
-            <th>Last Name</th>
-            <th>First Name</th>
-            <th>Deployment</th>
-            <th>MOS</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredSoldiers.map((soldier, index) => (
-            <tr key={index}>
-              <td>{soldier.last_name}</td>
-              <td>{soldier.first_name}</td>
-              <td>{soldier.id_deployments}</td>
-              <td>{soldier.id_mos.join(', ')}</td>
+        <br />
+        <table>
+          <thead>
+            <tr>
+              <th>Last Name</th>
+              <th>First Name</th>
+              <th>Deployment</th>
+              <th>MOS</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filteredSoldiers.map((soldier, index) => (
+              <tr key={index}>
+                <td>{soldier.last_name}</td>
+                <td>{soldier.first_name}</td>
+                <td>{soldier.id_deployments}</td>
+                <td>{soldier.id_mos.join(', ')}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
